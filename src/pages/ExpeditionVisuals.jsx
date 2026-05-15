5035697e5ad99f1788a970abf9c4e927483b9afe
DCLifeSafeGlobal <danielcaceresprieto@gmail.com>
2026-05-15 00:14:33 -0600

    Update and rename expedition-visuals.jsx to ExpeditionVisuals.jsx

--- src/pages/ExpeditionVisuals.jsx
+++ src/pages/ExpeditionVisuals.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const featuredImages = [
   {
-    src: "/dcp_visuals/colorado/colorado_redrocks_landscape_01.jpg",
+    src: "/dcp_visuals/colorado/colorado_redrocks_landscape_01.png",
     alt: "Colorado landscape",
   },
   {
-    src: "/dcp_visuals/colorado/colorado_deer_01.jpg",
+    src: "/dcp_visuals/colorado/colorado_deer_01.png",
     alt: "Colorado deer",
   },
   {
-    src: "/dcp_visuals/colorado/colorado_sunflower_bee_01.jpg",
+    src: "/dcp_visuals/colorado/colorado_sunflower_bee_01.png",
     alt: "Sunflower bee",
   },
   {
-    src: "/dcp_visuals/colorado/colorado_rockformation_animalhead.jpg",
+    src: "/dcp_visuals/colorado/colorado_rockformation_animalhead.png",
     alt: "Rock formation",
   },
 ];
