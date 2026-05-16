6e96c9824b5ff58b954ca40856922814672cf7c4
DCLifeSafeGlobal <danielcaceresprieto@gmail.com>
2026-05-16 16:58:13 -0600

    Update NewsletterForm.jsx

--- src/components/NewsletterForm.jsx
+++ src/components/NewsletterForm.jsx
@@ -1,1 +1,23 @@
-<script async data-uid=
+import { useEffect } from "react";
+
+export default function NewsletterForm() {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.async = true;
+    script.setAttribute("data-uid", "0e9f0f2615");
+    script.src = "https://dc-lifesafe-global.kit.com/0e9f0f2615/index.js";
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <section className="newsletter-section">
+      <div className="newsletter-container"></div>
+    </section>
+  );
+}
