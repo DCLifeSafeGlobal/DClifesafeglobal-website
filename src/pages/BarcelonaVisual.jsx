db8a08fd0dd1b3feb43fcf7127b77059886ceac0
DCLifeSafeGlobal <danielcaceresprieto@gmail.com>
2026-05-15 18:34:03 -0600

    Create BarcelonaVisual.jsx

---
+++ src/pages/BarcelonaVisual.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function BarcelonaVisual() {
+  return (
+    <div style={{ backgroundColor: "#000", minHeight: "100vh", padding: "120px 6vw 80px" }}>
+      <h1 style={{ color: "#fff", fontSize: "56px" }}>Barcelona Visual Archive</h1>
+    </div>
+  );
+}
