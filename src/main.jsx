cd76e78540c80ab3fa9ed8f7711818434aad8847
DCLifeSafeGlobal <danielcaceresprieto@gmail.com>
2026-05-17 19:24:57 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
+import { LanguageProvider } from './components/LanguageContext.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
