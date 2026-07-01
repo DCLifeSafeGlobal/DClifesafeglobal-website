743b6c636a578d7c092c8700ff9bd15b56ac29bb
DCLifeSafeGlobal <danielcaceresprieto@gmail.com>
2026-06-30 20:27:08 -0600

    Update RedRocksVisual.jsx

--- src/pages/RedRocksVisual.jsx
+++ src/pages/RedRocksVisual.jsx
@@ -1,1 +1,20 @@
-tokeep
+const coloradoPath =
+"/dcp_visuals/usa/colorado/colorado_springs/redrocks";
+
+const images = [
+
+`${coloradoPath}/dcp_redrocks_valley_view.png`,
+
+`${coloradoPath}/dcp_redrocks_deer_meadow.png`,
+
+`${coloradoPath}/dcp_redrocks_formation_panorama.png`,
+
+`${coloradoPath}/dcp_redrocks_lizard_macro.png`,
+
+`${coloradoPath}/dcp_redrocks_stone_animalhead_01.png`,
+
+`${coloradoPath}/dcp_redrocks_stone_animalhead_02.png`,
+
+`${coloradoPath}/dcp_redrocks_sunflower_macro.png`,
+
+];
